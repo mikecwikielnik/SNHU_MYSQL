973328bfceff08f0366d669cd6ec9df7c5e4e233
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-04-26 10:25:22 -0700

    Add files via upload

--- Data Analysis Checkpoints/5_1q.sql
+++ Data Analysis Checkpoints/5_1q.sql
@@ -3,7 +3,14 @@ CREATED BY: Mike Cwikielnik
 CREATED ON: 4/26/22
 DESCRIPTION: Code from Chapter 5,
 Data Analysis Checkpoint 1
-SQL Quickstart Guide
+Create a query for the invoices table that includes a CASE statement 
+that labels all sales from billing country USA as “Domestic Sales” 
+and all other sales as “Foreign Sales.” 
+Label your new field as SalesType after your END AS statement.
+
+Shields, Walter. 
+SQL QuickStart Guide: The Simplified Beginner's Guide to Managing, Analyzing, and Manipulating Data With SQL (p. 142).
+ClydeBank Media LLC. Kindle Edition. 
 */
 
 SELECT
--- Data Analysis Checkpoints/5_3q.sql
+++ Data Analysis Checkpoints/5_3q.sql
@@ -3,7 +3,11 @@ CREATED BY: Mike Cwikielnik
 CREATED ON: 4/26/22
 DESCRIPTION: Code from Chapter 5,
 Data Analysis Checkpoint 3
-SQL Quickstart Guide
+How many invoices from Domestic Sales were over $15?
+
+Shields, Walter. 
+SQL QuickStart Guide: The Simplified Beginner's Guide to Managing, Analyzing, and Manipulating Data With SQL (p. 142). 
+ClydeBank Media LLC. Kindle Edition. 
 */
 
 SELECT
