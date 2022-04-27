c228dd0c393c5d689f9b17180ddb7c979b4a3e64
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-04-27 06:01:35 -0700

    Add files via upload

--- ch_6.sql
+++ ch_6.sql
@@ -5,12 +5,41 @@ DESCRIPTION: Code from Chapter 6,
 SQL Quickstart Guide
 */
 
-SELECT 
-	* 
-FROM 
-	invoices
-INNER JOIN 
-	customers
-ON
-	INVOICES.CustomerId = customers.CustomerId
 	
+SELECT 
+	e.FirstName,
+	e.LastName,
+	e.EmployeeId,
+	c.FirstName,
+	c.LastName,
+	c.SupportRepId,
+	i.CustomerId,
+	i.Total
+FROM
+	invoices AS i
+JOIN
+	customers AS c
+ON
+	i.CustomerId = c.CustomerId
+INNER JOIN
+	employees as e
+ON
+	c.SupportRepId = e.EmployeeId
+ORDER BY
+	i.Total DESC
+LIMIT
+	10
+
+SELECT 
+	ar.ArtistId as [ArtistId From Artists Table],
+	al.ArtistId as [ArtistId From Albums Table],
+	ar.Name as [Artist Name],
+	al.Title as [Album Title]
+FROM
+	artists as ar
+LEFT OUTER JOIN
+	albums as al
+ON
+	ar.ArtistId = al.ArtistId
+WHERE 
+	al.ArtistId IS NULL
