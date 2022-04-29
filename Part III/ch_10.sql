b9f0f689b4f4f01ec52633b62ce732219fd4aaa4
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-04-29 07:38:08 -0700

    Add files via upload

---
+++ Part III/ch_10.sql
@@ -0,0 +1,11 @@
+/* 
+CREATED BY: Mike Cwikielnik
+DESCRIPTION: Code from Chapter 10,
+SQL Quickstart Guide
+*/
+
+-- INSERTING DATA INTO A DATABASE
+
+INSERT INTO
+artists (NAME)
+VALUES ('Bob Marley')
